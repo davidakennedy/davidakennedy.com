7e16c68a810e45403aee2cb92af813836a10a555
David A. Kennedy <me@davidakennedy.com>
2019-11-09 13:28:38 -0500

    Add better image handling and about page content * Also adds figure styles. * And basic image styles and spacing. * Plus more robust img shortcode.

--- .eleventy.js
+++ .eleventy.js
@@ -70,18 +70,13 @@ module.exports = function(eleventyConfig) {
 
   // Generate image markup.
   // A responsive image helper using Netlify Large Media - image transformation
-  eleventyConfig.addShortcode("img", (classname = "", url = "", alt = "") => {
-    return `<img class="${classname}"
-    srcset="/assets/img/${url}?nf_resize=fit&w=320 320w,
-      /assets/img/${url}?nf_resize=fit&w=640 640w,
-      /assets/img/${url}?nf_resize=fit&w=800 800w,
-      /assets/img/${url}?nf_resize=fit&w=1024 1024w,
-      /assets/img/${url}?nf_resize=fit&w=1280 1280w,
-      /assets/img/${url}?nf_resize=fit&w=1440 1440w,
-      /assets/img/${url}?nf_resize=fit&w=1920 1920w"
-    src="/assets/img/${url}?nf_resize=fit&w=600" alt="${alt}"
-    />`;
-  });
+  // Also not indented because: https://www.11ty.io/docs/languages/markdown/#there-are-extra-and-in-my-output
+  eleventyConfig.addShortcode(
+    "img",
+    (figclass = "", imgclass = "", url, alt = "", caption = "") => {
+      return `<figure ${figclass ? `class="${figclass}"` : ""}><img ${imgclass ? `class="${imgclass}"` : ""} srcset="/assets/img/${url}?nf_resize=fit&w=320 320w, /assets/img/${url}?nf_resize=fit&w=640 640w, /assets/img/${url}?nf_resize=fit&w=800 800w, /assets/img/${url}?nf_resize=fit&w=1024 1024w, /assets/img/${url}?nf_resize=fit&w=1280 1280w, /assets/img/${url}?nf_resize=fit&w=1440 1440w, /assets/img/${url}?nf_resize=fit&w=1920 1920w" src="/assets/img/${url}?nf_resize=fit&w=600" ${alt ? `alt="${alt}"` : alt === "" ? `alt="${alt}"` : ""} />${caption ? `<figcaption>${caption}</figcaption>` : ""}</figure>`;
+    }
+  );
 
   // Get current year for copyright.
   eleventyConfig.addShortcode("copyrightDates", startYear => {
--- _src/about.md
+++ _src/about.md
@@ -8,4 +8,21 @@ navtitle: About
 navorder: 2
 ---
 
-I am a person that writes stuff.
+{% img "wide-width", "", "23469199744_ea3ba2216f_o-1024x683.jpg", "David Kennedy speaking at WordCamp US 2015.", "" %}
+
+I work as a Senior UX Designer at [Ad Hoc](https://adhocteam.us/), focusing on accessibility.
+
+I'm an accessibility evangelist who loves the open web and open source code. I've contributed to [WordPress](https://wordpress.org/) and the WordPress community in the past. Previosly, I worked with great people at [Automattic](https://automattic.com/), [Chief](http://www.agencychief.com), The [Consumer Financial Protection Bureau](http://www.consumerfinance.gov/) and [The Arc](http://www.thearc.org/) doing all sorts of web stuff. In another life, I use to write words instead of code as a journalist.
+
+I root for the Florida Gators, dig CrossFit workouts, love peanut butter and jelly sandwiches, enjoy reading Raymond Carver, play video games too much and adore making my daughter smile. I have a lovely wife, Joeleen, one awesome daughter, Skye and one cool cat (Tigger).
+
+You can learn more about my professional experience by visiting my [Linkedin profile](http://www.linkedin.com/in/davidakennedy)</a>.
+
+## Other Useful Links
+
+- [Now](https://davidakennedy.com/now/)
+- [Speaking](http://davidakennedy.com/speaking/)
+- [Selected Writing](http://davidakennedy.com/category/selected-writing/)
+- [Contact](https://davidakennedy.com/contact/)
+
+<small>_Photo Credit: Casey Alexander, Creative Commons_</small>.
