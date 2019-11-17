a8d5411c9ada25837be8187baa9a239e1c9cff4b
David A. Kennedy <me@davidakennedy.com>
2019-11-16 20:08:34 -0500

    Include ISO date in `datetime` string

--- .eleventy.js
+++ .eleventy.js
@@ -25,7 +25,7 @@ module.exports = function(eleventyConfig) {
 
   // https://html.spec.whatwg.org/multipage/common-microsyntaxes.html#valid-date-string
   eleventyConfig.addFilter("htmlDateString", dateObj => {
-    return DateTime.fromJSDate(dateObj, { zone: "utc" }).toFormat("yyyy-LL-dd");
+    return DateTime.fromJSDate(dateObj, { zone: "utc" }).toISO();
   });
 
   // Get the first `n` elements of a collection.
