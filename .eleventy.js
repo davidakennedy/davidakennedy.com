4fc80aa09c3e935df57b951814fae0c3ae8e7a1d
David A. Kennedy <me@davidakennedy.com>
2022-10-29 22:17:19 -0400

    Add group by year filter and group posts

--- .eleventy.js
+++ .eleventy.js
@@ -73,22 +73,22 @@ module.exports = function (eleventyConfig) {
     return filterTagList([...tagSet]);
   });
 
-  // Create collection of posts by year
-  // https://github.com/11ty/eleventy/issues/1284#issuecomment-1026679407
-  eleventyConfig.addCollection("postsByYear", (collection) => {
-    const posts = collection.getFilteredByTag("posts").reverse();
-    const years = posts.map((post) => post.date.getFullYear());
-    const uniqueYears = [...new Set(years)];
+  // Group posts by year
+  // https://github.com/11ty/eleventy/discussions/2630
+  eleventyConfig.addFilter("groupByYear", function (pages = []) {
+    const pagesMap = {};
+    for (const page of [...pages]) {
+      const pageYear = page.date.getFullYear();
+      const yearlyPosts = pagesMap[pageYear] || [pageYear, []];
+      yearlyPosts[1].push(page);
+      pagesMap[pageYear] = yearlyPosts;
+    }
 
-    const postsByYear = uniqueYears.reduce((prev, year) => {
-      const filteredPosts = posts.filter(
-        (post) => post.date.getFullYear() === year
-      );
-
-      return [...prev, [year, filteredPosts]];
-    }, []);
-
-    return postsByYear;
+    return (
+      Object.values(pagesMap)
+        // Sort the year map in descending order.
+        .sort(([year1], [year2]) => year2 - year1)
+    );
   });
 
   // Development filters
