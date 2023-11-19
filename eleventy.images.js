4b7a4a61591c8c72e270bcc51ad4f710ec3919bc
David A. Kennedy <me@davidakennedy.com>
2023-11-19 10:11:46 -0500

    Make images wider than other content

--- eleventy.images.js
+++ eleventy.images.js
@@ -1,7 +1,7 @@
 const path = require("path");
 const image = require("@11ty/eleventy-img");
 
-module.exports = eleventyConfig => {
+module.exports = (eleventyConfig) => {
   function relativeToInputPath(inputPath, relativeFilePath) {
     let split = inputPath.split("/");
     split.pop();
@@ -10,29 +10,35 @@ module.exports = eleventyConfig => {
   }
 
   // Image shortcode for eleventy-img
-  eleventyConfig.addAsyncShortcode("image", async function imageShortcode(src, alt, caption) {
-    let metadata = await image(relativeToInputPath(this.page.inputPath, src), {
-      // Fix paths
-      urlPath: "/assets/media/",
-      outputDir: path.join(eleventyConfig.dir.output, "assets/media"),
-      widths: [407, 815, 1630],
-      formats: ["jpeg"],
-    });
+  eleventyConfig.addAsyncShortcode(
+    "image",
+    async function imageShortcode(src, alt, caption) {
+      let metadata = await image(
+        relativeToInputPath(this.page.inputPath, src),
+        {
+          // Fix paths
+          urlPath: "/assets/media/",
+          outputDir: path.join(eleventyConfig.dir.output, "assets/media"),
+          widths: [400, 800, 1600],
+          formats: ["jpeg"],
+        }
+      );
 
-    let imageAttributes = {
-      alt,
-      sizes: "(max-width: 835px) calc(100vw - 1em), 815px",
-      loading: "lazy",
-      decoding: "async",
-    };
+      let imageAttributes = {
+        alt,
+        sizes: "800px",
+        loading: "lazy",
+        decoding: "async",
+      };
 
-    if (caption !== undefined) {
-      return `<figure>${image.generateHTML(
-        metadata,
-        imageAttributes
-      )}<figcaption>${caption}</figcaption></figure>`;
-    } else {
-      return image.generateHTML(metadata, imageAttributes);
+      if (caption !== undefined) {
+        return `<figure>${image.generateHTML(
+          metadata,
+          imageAttributes
+        )}<figcaption>${caption}</figcaption></figure>`;
+      } else {
+        return image.generateHTML(metadata, imageAttributes);
+      }
     }
-  });
+  );
 };
