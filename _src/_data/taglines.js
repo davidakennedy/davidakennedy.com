3d6124b8181b3e2a62609029f48b03d48f99dda7
David A. Kennedy <me@davidakennedy.com>
2024-06-15 10:56:56 -0400

    Update taglines

--- _src/_data/taglines.js
+++ _src/_data/taglines.js
@@ -1,9 +1,12 @@
 module.exports = {
   phrases: [
-    "Probably eating a taco.",
-    "Watching basketball.",
-    "Peanut butter and jelly isn't just for kids.",
+    "Forever trying to get my dog and cat to cuddle.",
     "Too many books to read.",
+    "Throwing around a kettlebell.",
+    "Smashing a video game controller.",
+    "Probably eating a taco.",
+    "Peanut butter and jelly isn't just for kids.",
+    "Writing is thinking.",
     "Trust the process.",
   ],
 };
