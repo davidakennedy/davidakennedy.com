dfb9cbfef7849dcdd4728d6549a73ba411700caa
David A. Kennedy <me@davidakennedy.com>
2024-11-10 22:14:08 -0500

    Update metadata

--- _src/_data/metadata.js
+++ _src/_data/metadata.js
@@ -4,13 +4,6 @@ export default {
   description:
     "David A. Kennedy's personal website and blog about inclusive design, open source and life.",
   locale: "en_US",
-  feed: {
-    subtitle: "Inclusive Design, Open Source and Life.",
-    filename: "atom.xml",
-    path: "/feed/",
-    url: "https://davidakennedy.com/feed/",
-    id: "https://davidakennedy.com/",
-  },
   author: {
     name: "David A. Kennedy",
     email: "me@davidakennedy.com",
@@ -25,9 +18,9 @@ export default {
         name: "davidakennedy",
         url: "https://codepen.io/davidakennedy/",
       },
-      twitter: {
-        name: "davidakennedy",
-        url: "https://twitter.com/davidakennedy/",
+      bluesky: {
+        name: "davidakennedy.com",
+        url: "https://bsky.app/profile/davidakennedy.com",
       },
     },
   },
